895955d650f9aca47458d77d4e84f099e81df50f
Dimix-international <dima.dimix94@mail.ru>
2024-03-19 16:46:30 +0300

    main: models

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
+
+const (
+	port string = ":8989"
+)
 
 func main() {
-	fmt.Println("start")
+	http.HandleFunc("/", checkService)
+	if err := http.ListenAndServe(port, nil); err != nil {
+		log.Panic("error start serve" + err.Error())
+	}
+}
+
+func checkService(w http.ResponseWriter, r *http.Request) {
+	responseString(w, `{"success": true}`)
+}
+
+func responseString(w http.ResponseWriter, text string) {
+	responseJson(w, []byte(text))
+}
+
+func responseJson(w http.ResponseWriter, v []byte) {
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	if _, err := w.Write(v); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(`Error`))
+	}
 }
